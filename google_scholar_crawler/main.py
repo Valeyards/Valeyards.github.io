ba3aef13de8a4e37782845ffc3322b166c9dc486
Arviny <36869557+Valeyards@users.noreply.github.com>
2025-07-08 18:17:04 +0800

    fix [crawler bug](RayeRen/acad-homepage.github.io#125)

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -3,14 +3,31 @@ import jsonpickle
 import json
 from datetime import datetime
 import os
+import time
 
-# Setup proxy
-pg = ProxyGenerator()
-pg.FreeProxies()  # Use free rotating proxies
-scholarly.use_proxy(pg)
 
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+max_attempts = 100
+wait_seconds = 600  # 10 minutes
+
+for attempt in range(1, max_attempts + 1):
+    try:
+        print(f"Attempt {attempt}:")
+        # Setup proxy
+        pg = ProxyGenerator()
+        pg.FreeProxies()  # Use free rotating proxies
+        scholarly.use_proxy(pg)
+        
+        author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+        scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+        print(f"Attempt {attempt} success")
+        break  # Exit loop on first success
+    except Exception as e:
+        print(f"Attempt {attempt} failed with error: {e}")
+        time.sleep(wait_seconds)
+else:
+    print("All 100 attempts failed.")
+
+
 name = author['name']
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
